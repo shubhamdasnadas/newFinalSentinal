ade0fb59817fe18b8073ae1c829a06ecbc24fedd
@dalvis910 <dalvis910@gmail.com>
2026-06-16 18:50:36 +0530

    add cve changes

--- SoneTenancy/demoProjectTenant/multi-tenant-saas/app/dashboard/security/page.tsx
+++ SoneTenancy/demoProjectTenant/multi-tenant-saas/app/dashboard/security/page.tsx
@@ -1,5 +1,22 @@
-﻿import SecurityDashboard from "../../components/SecurityDashboard";
+﻿// import SecurityDashboard from "../../components/SecurityDashboard";
 
-export default function SecurityPage() {
-  return <SecurityDashboard />;
+// export default function SecurityPage() {
+//   return 
+//   <SecurityDashboard />;
+// }
+
+import SecurityDashboard from '@/app/components/SecurityDashboard'
+import React from 'react'
+import S1Agent from './S1Agent/S1Agent'
+import S1Cve from './S1CVE/S1Cve'
+const SecurityPage = () => {
+  return (
+    <div>
+      <SecurityDashboard />
+      <S1Agent />
+      <S1Cve />
+    </div>
+  )
 }
+
+export default SecurityPage
